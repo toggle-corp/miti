29f5a0bd649fe5f16a9aa837564ccc3377553da8
Bibek Dahal <bibekdahal.bd16@gmail.com>
2017-03-01 17:31:45 +0545

    Fix unicode problems

--- pullData.py
+++ pullData.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+
+
 from bs4 import BeautifulSoup
 import configparser
 import argparse
@@ -16,21 +19,23 @@ headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1)'
 
 
 def map_value(num):
-    number = [b'\xc3\xa0\xc2\xa5\xc2\xa6',  # 0
-              b'\xc3\xa0\xc2\xa5\xc2\xa7',  # 1
-              b'\xc3\xa0\xc2\xa5\xc2\xa8',  # 2
-              b'\xc3\xa0\xc2\xa5\xc2\xa9',  # 3
-              b'\xc3\xa0\xc2\xa5\xc2\xaa',  # 4
-              b'\xc3\xa0\xc2\xa5\xc2\xab',  # 5
-              b'\xc3\xa0\xc2\xa5\xc2\xac',  # 6
-              b'\xc3\xa0\xc2\xa5\xc2\xad',  # 7
-              b'\xc3\xa0\xc2\xa5\xc2\xae',  # 8
-              b'\xc3\xa0\xc2\xa5\xc2\xaf']  # 9
+    number = [
+        '०',
+        '१',
+        '२',
+        '३',
+        '४',
+        '५',
+        '६',
+        '७',
+        '८',
+        '९',
+    ]
     val = []
     # 3 -> 03
-    val.append('0') if len(num) == 3 else None
-    for i in range(0, len(num), 3):
-        val.append(str(number.index(num[i:i+3].encode('utf-8'))))
+    val.append('0') if len(num) == 2 else None
+    for i in range(0, len(num)):
+        val.append(str(number.index(num[i])))
     return ''.join(val)
 
 
@@ -38,14 +43,14 @@ def pull_data_from_month(year, month):
     """
     Year and month integer
     """
-    html_page = requests.get(web_url+str(year)+'/'+str(month)+'/',
-                             headers=headers)
+    html_page = requests.get(web_url+str(year)+'/'+str(month)+'/')
+    html_page.encoding = 'utf-8'
 
     if html_page.status_code is not 200:
         print('Internet Error!!')
         return
 
-    soup = BeautifulSoup(html_page.text, 'html.parser')
+    soup = BeautifulSoup(html_page.text, 'lxml')
     calender = soup.find('div', class_="calendar")
 
     if not calender:
@@ -83,21 +88,23 @@ def pull_data_from_month(year, month):
     return data
 
 
-def pull_data(init_year, end_year, dump='json.dumps'):
+def pull_data(init_year, end_year, dump='dump.json'):
     data = []
     for year in range(init_year, end_year+1):
         print('*'*30)
         print('Pulling For Year:', year)
         print('*'*30)
-        for month in range(1, 12+1):
+        for month in range(1, 13):
             print('\tPulling For Month:', month)
             data.extend(pull_data_from_month(year, month))
             print('\t'+'-'*25, 'Done', '-'*25)
 
     with open(dump, 'w') as file:
-        file.write(json.dumps({'init_year': init_year,
-                               'end_year': end_year,
-                               'data': data}))
+        json.dump({
+            'init_year': init_year,
+            'end_year': end_year,
+            'data': data
+        }, file)
         print('Data Dumped to:', dump)
 
 
@@ -109,7 +116,7 @@ if __name__ == '__main__':
                         help='End Month')
     parser.add_argument('--dump', metavar='-d', type=str,
                         help='Dump file location',
-                        default='dump/json.dumps')
+                        default='dump/dump.json')
     args = parser.parse_args()
 
     init_year = args.init_year
